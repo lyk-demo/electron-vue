3f80d884c5830d5342522b1f8452c95eb434c0b9
leiyunkang7@sina.com <leiyunkang7@sina.com>
2019-06-02 14:54:36 +0800

    add i18n element-ui

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import i18n from './i18n'
+import 'normalize.css'
+import ElementUI from 'element-ui'
+import './styles.scss'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
